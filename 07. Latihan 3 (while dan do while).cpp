05950a1778febe2903be5a523240bfb724ab8241
Ayse <106028948+helmimarif@users.noreply.github.com>
2022-05-23 10:24:05 +0700

    Loop II

---
+++ 07. Latihan 3 (while dan do while).cpp
@@ -0,0 +1,23 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int i=1;
+	
+		// Perulangan While
+	while (i<1)
+	{
+		cout<<"Belajar Perulangan While "<<i<<endl;
+		i++;
+	}
+	
+		// Perulangan Do While
+	do {
+		cout<<"Belajar Perulangan Do While "<<i<<endl;
+		i++;
+	} while (i<1);
+	
+	getch();
+}
